940b4f9648844403ba9f101e5a28d3c54e525246
Rohini Bali <64684079+Rohini-Bali@users.noreply.github.com>
2022-03-25 18:34:13 +0530

    make a custom component and style elements

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,21 @@
 //Filename for custom components should be in CapitalCamel case
 import './expenseItem.css'
+
+
+//to add dynamic variables  you should add js in function
 function ExpenseItem(){
+    //define three different vars for UI
+    const expenseDate = new Date(2022, 3,24) //<div>{expenseDate}</div> if you try like this it would break //toISOString method is required when working with Date obj
+    const expenseTitle = 'Books'
+    const expenseAmount = 123
     // per return you can only have one root element
     //to overcome this you can put all the elements in a parent division
     return(
         <div className='expense-item'>
-            <div>24 March 2022</div>
+            <div>{expenseDate.toISOString()}</div> 
             <div className='expense-item__description'>
-            <h2>Car insurance</h2>
-            <div className='expense-item__price'>$123</div>
+            <h2>{expenseTitle}</h2>
+            <div className='expense-item__price'>${expenseAmount}</div>
             </div>
         </div>
     );
