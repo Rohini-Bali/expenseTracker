2781d7d6e45e1bb880924c74b1838d9818d11398
Rohini Bali <64684079+Rohini-Bali@users.noreply.github.com>
2022-03-28 19:57:36 +0530

    Make state changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/Expenses/NewExpenses/NewExpense";
+
 function App() {
   const expenses = [
     { title: "Books", amount: 152, date: new Date(2022, 3, 25), id: 1 },
@@ -8,25 +10,11 @@ function App() {
   ];
   return (
     // <ExpenseItem></ExpenseItem> Either you can import the JSX like this or the other one that is given below
-
-    <>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date} />
-        <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date} /><>
-        <ExpenseItem
-            title={expenses[2].title}
-            amount={expenses[2].amount}
-            date={expenses[2].date} />
-        <ExpenseItem
-            title={expenses[3].title}
-            amount={expenses[3].amount}
-            date={expenses[3].date} /></>
-      </>
+    <div>
+    <NewExpense />
+    <Expenses expense={expenses}></Expenses>
+      </div>
+    
   )
 
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,5 +1,8 @@
 //Filename for custom components should be in CapitalCamel case
+import React, {useState} from 'react';
 import './expenseItem.css'
+import ExpenseDate from './ExpenseDate';
+import Card from '../common/Card';
 
 
 //to add dynamic variables  you should add js in function
@@ -8,16 +11,27 @@ function ExpenseItem(props) {
     // const expenseDate = new Date(2022, 3, 24) //<div>{expenseDate}</div> if you try like this it would break //toISOString method is required when working with Date obj
     // const expenseTitle = 'Books'
     // const expenseAmount = 123
+
+    //introduction to events and hooks
+    const [title,setTitle] = useState(props.title)
+    const  clickHandler = () =>{
+        setTitle('New title')
+        console.log('Clicked');
+    }
+    
+
+
     // per return you can only have one root element
     //to overcome this you can put all the elements in a parent division
     return (
-        <div className='expense-item'>
-            <div>{props.date.toISOString()}</div>
+        <Card className='expense-item'>
+            <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
-        </div>
+            <button onClick={clickHandler}>Make Changes</button>
+        </Card>
     );
 
 }
